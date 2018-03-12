149a491f49c72169a0ad12e1c9b96f8ed6df2766
Nikita Smirnov <smirn_n@yahoo.com>
2018-03-12 21:16:57 +0300

    Small changes to console output

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from src.parser import Parser
 import sys
 import os
@@ -7,7 +8,8 @@ def main():
     if (len(sys.argv) < 2):
         print("Enter assembler file name")
         print("Example: ")
-        print(f"         {sys.argv[0]} file.asm")
+        print(f"{os.path.basename(sys.argv[0])} file.asm")
+        input("Press Enter to exit...")
     asmFile = open(sys.argv[1], 'r')
     asmFileName = os.path.splitext(sys.argv[1])[0]
     asmProg = asmFile.readlines()
